0c0d4e9305cc9f7f2388875a04bc725427d804e3
Jim Liu 宝玉 <junminliu@gmail.com>
2022-11-23 19:56:33 -0600

    Correct the folder name of Button

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,3 @@
-import { Button } from "@community-next/design-system/Button.tsx/Button";
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <div>
-      <Button size="sm">hello</Button>
-    </div>
-  );
+  return <div></div>;
 }
